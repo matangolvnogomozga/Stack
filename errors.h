cc3e54775c32e88e7ad2f7c68649854cb47b3798
Игорь Оберман <56135465+matangolvnogomozga@users.noreply.github.com>
2019-11-15 04:15:48 +0300

    Create errors.h

---
+++ errors.h
@@ -0,0 +1,12 @@
+enum ERRORS
+{
+    ALL_GOOD    = 0,
+    ERR_CAP     = 1,
+    ERR_SIZE    = 2,
+    OVER_FLOW   = 3,
+    EMPTY       = 4,
+    ERR_CANARY  = 5,
+    ERR_HASH    = 6,
+    NO_MEM_MORE = 7,
+    ERR_PTR     = 8
+};
