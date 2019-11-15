a67cec425f8bda36801f1427496904b73a2c7fda
Игорь Оберман <56135465+matangolvnogomozga@users.noreply.github.com>
2019-11-15 04:12:40 +0300

    Create declaration.h

---
+++ declaration.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include<windows.h>
+#include<cassert>
+#include<math.h>
+
+long long RandomBig();
+void SetColour(ConsoleColor text, ConsoleColor background);
+
+const bool FAIL = false;
+const bool SUCCESS = true;
+const int LEN = 12;
+const long long CANARYBEGIN1 = RandomBig();
+const long long CANARYBEGIN2 = RandomBig();
