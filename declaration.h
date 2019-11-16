8e0e6d0ebbae4ee8789a41e1f12c54ad49e41b9e
Игорь Оберман <56135465+matangolvnogomozga@users.noreply.github.com>
2019-11-16 11:17:36 +0300

    Update declaration.h

--- declaration.h
+++ declaration.h
@@ -6,6 +6,8 @@
 #include<cassert>
 #include<math.h>
 
+//#define UNSAFE /// turn off safety
+
 #include<functions.h>
 
 long long RandomBig();
@@ -14,5 +16,7 @@ void SetColor(ConsoleColor text, ConsoleColor background);
 const bool FAIL = false;
 const bool SUCCESS = true;
 const int LEN = 12;
+#ifndef UNSAFE
 const long long CANARYBEGIN1 = RandomBig();
 const long long CANARYBEGIN2 = RandomBig();
+#endif // UNSAFE
