f9a5ea795a263e75f8dcba02be0f1663c4d79d99
Игорь Оберман <56135465+matangolvnogomozga@users.noreply.github.com>
2019-11-20 19:31:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main()
     if(size > cap)
     {
         printf("Wrong entry");
-        return 0;
+        return 1;
     }
 
     /// Test functions
@@ -28,7 +28,7 @@ int main()
     Stack<int> Stack;
     bool check = Stack.Create(cap);
     if(check == FALSE)
-        return 0;
+        return 1;
 
     /// Fill the stack with random numbers
     /// Test PUSH
@@ -36,37 +36,37 @@ int main()
     for(int i = 0; i < size; ++i)
         check = Stack.Push(rand() % 10);
     if(check == FALSE)
-        return 0;
+        return 1;
     /// Printing all stack characteristics and elements
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
 
     /// Test POP
     int num = 0;
     Stack.Pop(&num);
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Here is the last element of the stack: %d\n", num);
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Pay attention that last element isn't in stack\n");
 
     /// Test SIZE
     check = Stack.Size(&num);
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Now there is only %d elements in stack\n", num);
 
     /// Test GET
     check = Stack.Get(&num);
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Here is the last element of the stack: %d\n", num);
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Pay attention that last element is still in stack\n");
 
     /// Test.2 PUSH
@@ -75,55 +75,55 @@ int main()
     if(check == FALSE)
     {
         printf("Wrong entry");
-        return 0;
+        return 1;
     }
     check = Stack.Push(num);
     if(check == FALSE)
-        return 0;
+        return 1;
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
 
 
     /// Test CLEAR
     check = Stack.Clear();
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Now stack is empty\n");
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
 
     /// Test-2 SIZE
     check = Stack.Size(&num);
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Now stack is empty (size = %d)\n", num);
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
 
     /// Test PLUSMEMORY
     for(int i = 0; i < size; ++i)
         check = Stack.Push(rand() % 10);
     if(check == FALSE)
-        return 0;
+        return 1;
     /// Now stack is overflow, but function will get memory
     for(int i = 0; i < size; ++i)
         check = Stack.Push(rand() % 10);
     if(check == FALSE)
-        return 0;
+        return 1;
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
 
     /// Test DESTROY
     check = Stack.Destroy();
     if(check == FALSE)
-        return 0;
+        return 1;
     printf("Stack isn't exist now\n");
     check = Stack.Dump();
     if(check == FALSE)
-        return 0;
+        return 1;
     return 0;
 }
